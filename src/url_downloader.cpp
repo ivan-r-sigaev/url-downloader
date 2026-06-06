c07b55a66a887854f4c5d6cb07978e64a0c49386
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2026-06-06 20:51:49 +0300

    Refactor reading URLs from file

--- src/url_downloader.cpp
+++ src/url_downloader.cpp
@@ -7,12 +7,21 @@
 #include <algorithm>
 #include <cctype>
 
-/// Parses input file into strings line by line. Empty lines are ignored.
-std::vector<std::string> read_lines(std::filesystem::path urls_path) {
+/// Parses input file into url strings line by line. Empty lines are ignored.
+std::vector<std::string> read_urls(std::filesystem::path urls_path) {
     std::ifstream fs(urls_path.c_str(), std::fstream::in | std::fstream::binary);
     std::vector<std::string> out = {};
     for (std::string line; std::getline(fs, line);) {
+        // Skip whitespace.
         if (std::all_of(line.begin(), line.end(), isspace)) {
+            continue;
+        }
+        // Skip non-HTTP/HTTPS protocols.
+        if (line.rfind("https://", 0) != 0 || line.rfind("http://", 0) != 0) {
+            std::cerr 
+                << "Warning: URL skipped; Reason: unknown protocol, must be HTTP/HTTPS; URL: "
+                << line
+                << '\n';
             continue;
         }
         out.push_back(line);
@@ -26,7 +35,7 @@ int main(int argc, char* argv[]) {
         if (argc >= 1) {
             command_name = std::string(argv[0]);
         }
-        std::cerr << "Usage: " << command_name << " <urls_file> <out_dir> <parallel_download_count>" << std::endl;
+        std::cerr << "Usage: " << command_name << " <urls_file> <out_dir> <parallel_download_count>\n";
 
         exit(EXIT_FAILURE);
     }
@@ -34,7 +43,8 @@ int main(int argc, char* argv[]) {
     auto out_dir_path = std::filesystem::path(argv[2]);
     auto parallel_download_count = std::stoi(std::string(argv[3]));
 
-    auto urls = read_lines(urls_path);
+    auto urls = read_urls(urls_path);
+
 
     // TODO: download files over http...
 
