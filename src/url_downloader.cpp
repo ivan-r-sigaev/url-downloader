b56f89edd3385bf328dfc53e0eea5c722a58ec49
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2026-06-10 12:44:55 +0300

    Implement URL downloading

--- src/url_downloader.cpp
+++ src/url_downloader.cpp
@@ -6,13 +6,27 @@
 #include <vector>
 #include <algorithm>
 #include <cctype>
+#include <curl/curl.h>
+#include <chrono>
+#include <sstream>
+
+/// Handle to manage an instance of downloading a file from URL.
+class DownloadHandle {
+public:
+    std::filesystem::path out_path{};
+    std::ofstream out_file{};
+    bool has_started = false;
+    std::chrono::steady_clock::time_point start_time{};
+public:
+    explicit DownloadHandle(std::filesystem::path _out_path) : out_path(_out_path) {}
+};
 
 /// Parses input file into url strings line by line. Empty lines are ignored.
-std::vector<std::string> read_urls(std::filesystem::path urls_path) {
+static std::vector<std::string> read_urls(const std::filesystem::path& urls_path) {
     std::ifstream fs(urls_path.c_str(), std::fstream::in | std::fstream::binary);
     std::vector<std::string> out = {};
     for (std::string line; std::getline(fs, line);) {
-        // Skip whitespace.
+        // Skip empty lines.
         if (std::all_of(line.begin(), line.end(), isspace)) {
             continue;
         }
@@ -29,7 +43,112 @@ std::vector<std::string> read_urls(std::filesystem::path urls_path) {
     return out;
 }
 
+static void sanitize_filename(std::string& filename) {
+    // TODO: implement this...
+}
+
+static size_t header_callback(char *buffer, size_t size, size_t nitems, void *userdata) {
+    auto handle = static_cast<DownloadHandle*>(userdata);
+
+    if (!handle->has_started) {
+        handle->start_time = std::chrono::steady_clock::now();
+        handle->has_started = true;
+        // TODO: print time...
+    }
+
+    std::string header(buffer, size * nitems);
+    auto delim = header.find(':');
+    if (delim != std::string::npos) {
+        auto key = header.substr(0, delim);
+        auto value = header.substr(delim + 1);
+        auto filename = std::string();
+        std::transform(
+            value.begin(), value.end(), value.begin(), 
+            [](unsigned char c){ return std::tolower(c); }
+        );
+        if (key == "content-disposition") {
+            auto sstream = std::stringstream(value);
+            auto token = std::string();
+
+            while (std::getline(sstream, token, ';')) {
+                const auto whitespace = " \t";
+                token = token.substr(token.find_first_not_of(whitespace));
+                const auto filename_field = std::string("filename=");
+                if (token.rfind(filename_field) != std::string::npos) {
+                    token = token.substr(filename_field.size());
+                    if (!token.empty() && token[0] == '"' && token.back() == '"') {
+                        token = token.substr(1, token.length() - 2);
+                    }
+                    sanitize_filename(token);
+                    filename = token;
+                }
+                const auto utf8_filename_field = std::string("filename*=");
+                if (token.rfind(utf8_filename_field) != std::string::npos) {
+                    // TODO: implement this...
+                }
+            }
+        }
+    }
+    return size * nitems;
+}
+
+static void create_file(std::ofstream& fs, std::filesystem::path path) {
+    fs.open(path, std::fstream::out | std::fstream::binary);
+    if (!fs.good()) {
+        return;
+    }
+
+    int postfix = 1;
+    std::string filename = path.filename().string();
+    size_t delim = filename.rfind('.');
+    std::string extension = filename.substr(delim);
+    filename = filename.substr(0, delim);
+    do {
+        path.replace_filename(filename + "(" + std::to_string(postfix) + ")" + extension);
+        fs.open(path, std::fstream::out | std::fstream::binary);
+        postfix++;
+    } while (!fs.good());
+}
+
+static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    auto handle = static_cast<DownloadHandle*>(userdata);
+
+    if (!handle->out_file.is_open()) {
+        create_file(handle->out_file, handle->out_path);
+    }
+
+    // Should I check success?
+    handle->out_file.write(ptr, nmemb);
+
+    return nmemb;
+}
+
+static void decode_url(std::string& text) {
+    int out_size = 0;
+    auto cstr = curl_easy_unescape(nullptr, text.c_str(), 0, &out_size);
+    if (cstr) {
+        text = std::string(cstr, out_size);
+        curl_free(cstr);
+    }
+}
+
+static std::string filename_from_url(const std::string& url) {
+    auto out = url.substr(0, url.find('#'));
+    out = out.substr(0, out.find('?'));
+    out = out.substr(out.rfind('/'));
+    if (out.empty()) {
+        out = "placeholder.txt";
+    } else {
+        decode_url(out);
+        sanitize_filename(out);
+    }
+    return out;
+}
+
 int main(int argc, char* argv[]) {
+    // TODO: print time...
+    curl_global_init(CURL_GLOBAL_ALL);
+
     if (argc < 4) {
         std::string command_name = "url_downloader";
         if (argc >= 1) {
@@ -37,16 +156,75 @@ int main(int argc, char* argv[]) {
         }
         std::cerr << "Usage: " << command_name << " <urls_file> <out_dir> <parallel_download_count>\n";
 
-        exit(EXIT_FAILURE);
+        std::exit(EXIT_FAILURE);
     }
     auto urls_path = std::filesystem::path(argv[1]);
     auto out_dir_path = std::filesystem::path(argv[2]);
     auto parallel_download_count = std::stoi(std::string(argv[3]));
+    // TODO: print arguments...
 
-    auto urls = read_urls(urls_path);
+    auto handles = std::vector<DownloadHandle>();
+    CURLM* multi_handle = curl_multi_init();
+    curl_multi_setopt(multi_handle, CURLMOPT_MAX_TOTAL_CONNECTIONS, (long)parallel_download_count);
+    {
+        auto urls = read_urls(urls_path);
+        for (const auto& url : urls) {
+            auto out_path = out_dir_path;
+            out_path.append(filename_from_url(url));
 
+            handles.emplace_back(out_path);
+            auto easy_handle = curl_easy_init();
+            curl_easy_setopt(easy_handle, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(easy_handle, CURLOPT_HEADERFUNCTION, header_callback);
+            curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, &handles.back());
+            curl_easy_setopt(easy_handle, CURLOPT_HEADERDATA, &handles.back());
+            curl_easy_setopt(easy_handle, CURLOPT_PRIVATE, &handles.back());
+            curl_multi_add_handle(multi_handle, easy_handle);
+        }
+    }
+    
+    int running_handles_count = 0;
+    do {
+        curl_multi_perform(multi_handle, &running_handles_count);
+        curl_multi_wait(multi_handle, nullptr, 0, 1000, nullptr);
 
-    // TODO: download files over http...
+        while (true) {
+            int msgsLeft = 0;
+            CURLMsg* msg = curl_multi_info_read(multi_handle, &msgsLeft);
+            if (!msg) {
+                break;
+            }
+            if (msg->msg == CURLMSG_DONE) {
+                // auto result = msg->data.result;
+                auto now = std::chrono::steady_clock::now();
+                auto easy_handle = msg->easy_handle;
+                long http_code = 0;
+                DownloadHandle* handle = nullptr;
+                curl_easy_getinfo(easy_handle, CURLINFO_RESPONSE_CODE, &http_code);
+                curl_easy_getinfo(easy_handle, CURLINFO_PRIVATE, &handle);
+                handle->out_file.close();
+                curl_multi_remove_handle(multi_handle, easy_handle);
+                curl_easy_cleanup(easy_handle);
+                if (http_code != 200) {
+                    std::cerr 
+                        << "Error: Failed to obtain URL contents; Code: "
+                        << http_code 
+                        << "; URL: "
+                        << handle->out_path 
+                        << '\n';
+                } else {
+                    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - handle->start_time).count();
+                    std::cout << handle->out_path << " - finished in " << elapsed << "ms\n";
+                }
+            }
+            std::cout << std::flush;
+        }
+    } while (running_handles_count > 0);
 
+    curl_multi_cleanup(multi_handle);
+    curl_global_cleanup();
+
+    // TODO: print time...
     return 0;
 }
